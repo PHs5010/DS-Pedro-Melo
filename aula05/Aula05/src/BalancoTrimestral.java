ab1863bd77f3476276a7016c721e9fbbbb1c76e0
Pedro Melo <pmelo9905@gmail.com>
2025-04-28 08:59:34 -0300

    Estudos sobre o NetBeans e Java

---
+++ aula05/Aula05/src/BalancoTrimestral.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class BalancoTrimestral {
+public static void main(String[]args){  
+  int gastoJaneiro = 15000;
+  int gastoFevereiro = 23000;
+  int gastoMarco = 17000;
+  
+  int gastoTrimestre = gastoJaneiro + gastoFevereiro + gastoMarco;
+  
+  System.out.println("O gasto total no trimestre foi de: R$" + gastoTrimestre);
+  }
+}
